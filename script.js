77b177e3a28c5712e4c39bb5867f346a1ac195bf
pavel_arseyev <pavel.arseev@bigdropinc.com>
2019-04-25 18:12:56 +0300

    refactored code

--- script.js
+++ script.js
@@ -1,130 +1,129 @@
-class ScanMe{
+class ScanMe {
+    constructor(item) {
+        this.section = item;
 
-  constructor(item){
-    this.section = item;
+        this.scanner = item.querySelector(".scanner-wrapper");
+        this.bgItem = item.querySelector(".scanner-bg");
 
-    this.scanner = item.querySelector(".scanner-wrapper");
-    this.bgItem = item.querySelector(".scanner-bg");
+        this.mousePressed = false;
 
-    this.mousePressed = false;
+        this.window = {
+            width: window.innerWidth,
+            height: window.innerHeight
+        };
 
-    this.window = {
-      width: window.innerWidth,
-      height: window.innerHeight
-    },
+        this.parent = {
+            moveX: 0,
+            moveY: 0,
+            clickX: 0,
+            clickY: 0,
+            width: this.section.offsetWidth,
+            height: this.section.offsetHeight
+        };
 
-    this.parent = {
-      moveX: 0,
-      moveY: 0,
-      clickX: 0,
-      clickY: 0,
-      width: this.section.offsetWidth,
-      height: this.section.offsetHeight
-    };
+        this.child = {
+            clickX: 0,
+            clickY: 0,
+            width: this.scanner.offsetWidth,
+            height: this.scanner.offsetHeight
+        };
 
-    this.child = {
-      clickX: 0,
-      clickY: 0,
-      width: this.scanner.offsetWidth,
-      height: this.scanner.offsetHeight
-    };
+        this.bg = {
+            posX: 0 || this.setBgPosition(),
+            posY: 0 || this.setBgPosition()
+        };
+    }
 
-    this.bg = {
-      posX: 0 || this.setBgPosition(),
-      posY: 0 || this.setBgPosition()
-    };
-  }
+    calcScannerClick() {
 
-  calcScannerClick(){
+    }
 
-  }
+    calcParentClick() {
 
-  calcParentClick(){
+    }
 
-  }
+    calcCursorMove() {
 
-  calcCursorMove(){
+    }
 
-  }
+    setBgPosition() {
 
-  setBgPosition(){
+    }
 
-  }
+    moveScanner() {
 
-  moveScanner(){
+    }
 
-  }
+    //Добавляет обработчики событий при загрузке страницы
+    addListeners() {
+        //Отслеживает размер окна
+        window.addEventListener("resize", () => {
+            let newWindowWidth = window.innerWidth;
+            let newWindowHeigt = window.innerHeight;
 
-  //Добавляет обработчики событий при загрузке страницы
-  addListeners(){
-    //Отслеживает размер окна
-    window.addEventListener('resize', () => {
-      let newWindowWidth = window.innerWidth;
-      let newWindowHeigt = window.innerHeight;
+            //Вычисляет новые значения при изменении размера экрана
+            if (newWindowWidth !== this.window.width || newWindowHeigt !== this.window.height) {
+                if (newWindowWidth !== this.window.width) {
+                    this.window.width = newWindowWidth;
+                    // console.log("Поменялась ширина");
+                }
 
-      //Вычисляет новые значения при изменении размера экрана
-      if(newWindowWidth !== this.window.width || newWindowHeigt !== this.window.height){
-        if(newWindowWidth !== this.window.width){
-          this.window.width = newWindowWidth;
-          // console.log("Поменялась ширина");
-        }
+                if (newWindowHeigt !== this.window.height) {
+                    this.window.height = newWindowHeigt;
+                    // console.log("Поменялась высота");
+                }
 
-        if(newWindowHeigt !== this.window.height){
-          this.window.height = newWindowHeigt;
-          // console.log("Поменялась высота");
-        }
+                this.parent.width = this.section.offsetWidth;
+                this.parent.height = this.section.offsetHeight;
+                this.child.width = this.scanner.offsetWidth;
+                this.child.height = this.scanner.offsetHeight;
 
-        this.parent.width = this.section.offsetWidth;
-        this.parent.height = this.section.offsetHeight;
-        this.child.width = this.scanner.offsetWidth;
-        this.child.height = this.scanner.offsetHeight;
+                this.moveScanner();
+                this.setBgPosition();
+            }
+        });
 
-        this.moveScanner();
-        this.setBgPosition();
-      }
-    });
+        //Считает координаты движения мыши по секции
 
-    //Считает координаты движения мыши по секции
+        //TODO: выяснить как снимать координаты именно с ролителя;
+        this.section.addEventListener('mousemove', (event) => {
+            if (this.mousePressed) {
+                this.parent.moveX = event.offsetX;
+                this.parent.moveY = event.offsetY;
 
-    //TODO: выяснить как снимать координаты именно с ролителя;
-    this.section.addEventListener('mousemove', (event) => {
-      if(this.mousePressed){
-        this.parent.moveX = event.offsetX;
-        this.parent.moveY = event.offsetY;
+                console.log("курсор на родителе по X: " + this.parent.moveX, "курсор на родителе по Y: " + this.parent.moveY);
+            }
+        });
 
-        console.log("курсор на родителе по X: " + this.parent.moveX, "курсор на родителе по Y: " + this.parent.moveY);
-      }
-    });
+        //Смотрит где я кликнул внутри секции
+        //TODO: выяснить как снимать координаты именно с ролителя;
+        this.section.addEventListener('mousedown', (event) => {
+            this.mousePressed = true;
 
-    //Смотрит где я кликнул внутри секции
-    //TODO: выяснить как снимать координаты именно с ролителя;
-    this.section.addEventListener('mousedown', (event) => {
-      this.mousePressed = true;
-  
-      this.parent.clickX = event.offsetX;
-      this.parent.clickY = event.offsetY;
-  
-      // console.log("ролитель Х: " + this.parent.clickX, "родитель Y: " + this.parent.clickY);  
-      console.log(event);  
-    });
+            this.parent.clickX = event.offsetX;
+            this.parent.clickY = event.offsetY;
 
-    //Выключает просчет движения мыши по секции
-    this.section.addEventListener('mouseup', () => {
-      this.mousePressed = false;
-    });
+            // console.log("ролитель Х: " + this.parent.clickX, "родитель Y: " + this.parent.clickY);
+            console.log(event);
+        });
 
-    this.section.addEventListener('mouseout', () => {
-      this.mousePressed = false;
-    });
+        //Выключает просчет движения мыши по секции
+        this.section.addEventListener('mouseup', () => {
+            this.mousePressed = false;
+        });
+
+        this.section.addEventListener('mouseout', () => {
+            this.mousePressed = false;
+        });
 
 
-  }
+    }
 
-  init(){
-    this.addListeners();
+    init() {
+        this.addListeners();
 
-    console.log(this); 
-  }
+        console.log(this);
+    }
 }
 
 let some = new ScanMe(document.querySelector(".scanner"));
